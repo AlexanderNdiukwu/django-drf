895d9e3fb11d27971b4ac2d3b9432a24b2895de2
AlexanderNdiukwu <ndiukwuchukwuemeka@gmail.com>
2025-03-07 14:10:27 +0100

    'add'

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from .models import Todo
+# from django.contrib.auth import get_user_model
 
 
 
-class todoSerializer(serializers.ModelSerializer):
+
+class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = '__all__'
+
+# class ProfileSerializer(serializers.ModelSerializer):
+#     class Meta :
+#         model = get_user_model()
+#         fields = ['username','profile_pic','bio','phone_number','email']
