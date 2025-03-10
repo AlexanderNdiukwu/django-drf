62bdd24755c10d7b889775001e242caa0732857c
AlexanderNdiukwu <ndiukwuchukwuemeka@gmail.com>
2025-03-10 23:59:14 +0100

    'add'

--- api/models.py
+++ api/models.py
@@ -2,17 +2,24 @@ from django.db import models
 
 
 
+class Status (models.Model):
+    status_choices = [
+        ('important','important'),
+        ('not important','not important'),
+        ('not sure','not sure')
+    ]
+    status_id = models.AutoField(primary_key=True)
+    status_input = models.CharField(max_length=120,choices=status_choices,default='not sure')
+
+    def __str__(self):
+        return self.status_input
 
 
 
 class Todo(models.Model):
     title = models.CharField(max_length=120)
+    status = models.ForeignKey(Status,on_delete=models.CASCADE , null=True, blank=True)
     description = models.TextField()
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    
-    def __str__(self):
-        return self.title
-    
--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,10 @@ from .models import Todo
 
 
 
+
+
 class TodoSerializer(serializers.ModelSerializer):
+    
 
     class Meta:
         model = Todo
